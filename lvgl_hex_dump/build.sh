8b37e494b8996fd192d29686a4b9316ea3dd32fb
.Replicant~ <102823475+JOKKEU@users.noreply.github.com>
2025-05-01 07:31:52 +0300

    build.sh

--- lvgl_hex_dump/build.sh
+++ lvgl_hex_dump/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo gcc src/main.c src/file_op.c src/init.c src/lvgl.c -o file_dump -D DBG -I/home/jokkeu/Desktop/codes/test_project -L/home/jokkeu/Desktop/codes/test_project/lvgl/build/lib -L/home/jokkeu/lvgl/lv_drivers/lib -I/usr/include/SDL2 -L/usr/lib/x86_64-linux-gnu -lSDL2 -llvgl -lm
+sudo gcc src/main.c src/file_op.c src/init.c src/lvgl.c -o file_dump -D DBG -I/home/jokkeu/Desktop/codes/lvgl_hex_dump -L/home/jokkeu/Desktop/codes/lvgl_hex_dump/lvgl/build/lib  -I/usr/include/SDL2 -L/usr/lib/x86_64-linux-gnu -lSDL2 -llvgl -lm
 
 
 
