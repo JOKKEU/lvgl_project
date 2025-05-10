f493eb9943ee158435c3873015ea390a920901c6
.Replicant~ <102823475+JOKKEU@users.noreply.github.com>
2025-05-10 23:53:40 +0300

    Delete lvgl_hex_dump directory

--- lvgl_hex_dump/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo gcc src/main.c src/file_op.c src/init.c src/lvgl.c -o file_dump -D DBG -I/home/jokkeu/Desktop/codes/lvgl_hex_dump -L/home/jokkeu/Desktop/codes/lvgl_hex_dump/lvgl/build/lib  -I/usr/include/SDL2 -L/usr/lib/x86_64-linux-gnu -lSDL2 -llvgl -lm
-
-
-
-
-
