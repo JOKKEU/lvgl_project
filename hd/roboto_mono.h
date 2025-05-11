481da852017d49bcb4090049a9debae6e4e6f80d
.Replicant~ <102823475+JOKKEU@users.noreply.github.com>
2025-05-12 01:23:08 +0300

    Add files via upload

---
+++ hd/roboto_mono.h
@@ -0,0 +1,14 @@
+#ifndef ROBOTO_MONO
+#define ROBOTO_MONO
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const lv_font_t roboto_mono; // Декларация шрифта
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ROBOTO_MONO
