daf47ffaac4d143f669be45ede4d4aa5eaa8051c
.Replicant~ <102823475+JOKKEU@users.noreply.github.com>
2025-05-12 00:54:48 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo gcc src/main.c src/file_op.c src/init.c src/lvgl.c -o  file_dump -D DBG  -I/home/jokkeu/Desktop/codes/lvgl_project -L/home/jokkeu/Desktop/codes/lvgl_project/lvgl/build/lib  -I/usr/include/SDL2 -L/usr/lib/x86_64-linux-gnu -lSDL2 -llvgl -lm
+sudo gcc src/main.c src/file_op.c src/init.c src/lvgl.c src/aovel_sans_rounded_font.c -o  file_dump -D DBG  -I/home/jokkeu/Desktop/codes/lvgl_project -L/home/jokkeu/Desktop/codes/lvgl_project/lvgl/build/lib  -I/usr/include/SDL2 -L/usr/lib/x86_64-linux-gnu -lSDL2 -llvgl -lm
 
 
 
