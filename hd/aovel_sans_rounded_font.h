8c151703d61246b5f3378493a9aae720041a5ead
.Replicant~ <102823475+JOKKEU@users.noreply.github.com>
2025-05-12 00:55:55 +0300

    Add files via upload

---
+++ hd/aovel_sans_rounded_font.h
@@ -0,0 +1,14 @@
+#ifndef AOVEL_SANS_ROUNDED_FONT
+#define AOVEL_SANS_ROUNDED_FONT
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const lv_font_t aovel_sans_rounded_font; // Декларация шрифта
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // AOVEL_SANS_ROUNDED_FONT
